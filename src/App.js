ee195ffb874717476ca12373185e18c51ba40a40
bardadymr <bardadymr@gmail.com>
2022-10-26 10:32:33 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
+
+  
+  // Counter
+  const [count, setCount] = React.useState(0)
+
+  const onClickPlus = () => {
+    setCount(count + 1)
+  }
+
+  const onClickMinus = () => {
+    setCount(count - 1)
+  }
+
+  const onClickMult = () => {
+    setCount(count*2)
+  }
+
+  const onClickReduce = () => {
+    setCount(count/2)
+  }
+
+  // Open Modal
+  const [open, setOpen] = React.useState(false);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div className="App">
+    <div className="open">
+      <button onClick={()=> setOpen(true)} className="openmodal">Open Counter</button>
+    </div>
+
+   {open && (
+    <div className="counter"> 
+      <button onClick={()=> setOpen(false)} className="close">Close</button>
+      <h2>Counter</h2>
+      <h1>{count}</h1>
+      <button onClick={onClickMinus} className="minus">Minus</button>
+      <button onClick={onClickPlus} className="plus">Plus</button>
+      <br/>
+      <button onClick={onClickMult} className="mult">X2</button>
+      <button onClick={onClickReduce} className="reduce">/2</button>
+    </div>
+   )}
     </div>
   );
 }
